fa2da736c84455224a2597dc0d75a518c8f667dd
CegepJulienCyr <julien.d.cyr@gmail.com>
2017-11-13 10:08:08 -0500

    Add files via upload

---
+++ Projet_POO_Debut/CombustiblesFossiles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_POO_intermediare_PotBa_CyrJu
+{
+    class CombustiblesFossiles
+    {
+
+        protected int QuantiteM;
+        protected int TempDechargement;
+        protected int TailleMax;
+        protected CPile ancre;
+
+       CombustiblesFossiles(int TailleMaxP, int QuantiteMP =0, CPile ancreP = null) base : (TailleMaxP , QuantiteMP, ancreP){
+
+       	
+        }
+
+    }
+}
