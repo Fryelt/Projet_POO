bc44da4a13f1e8160e1ca2fda555d14dcf06b02a
Bajamin Potvin <bajamin_28@hotmail.com>
2017-11-13 09:48:00 -0500

    k 2.0

---
+++ Projet_POO_intermediare_PotBa_CyrJu/Projet_POO_intermediare_PotBa_CyrJu/MétauxLourds.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_POO_intermediare_PotBa_CyrJu
+{
+    class MétauxLourds
+    {
+    }
+}
