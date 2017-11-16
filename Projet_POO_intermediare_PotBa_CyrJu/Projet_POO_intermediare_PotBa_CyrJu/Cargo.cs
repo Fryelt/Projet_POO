cf3e7c3723a87d7795885991dbf4056ae0125bd2
Bajamin Potvin <bajamin_28@hotmail.com>
2017-11-16 12:00:22 -0500

    Revert "Revert "Merge branch 'Bajamin'""
    This reverts commit 982516cf1e26a1cb273fdb065e0e1f7372532657.

--- Projet_POO_intermediare_PotBa_CyrJu/Projet_POO_intermediare_PotBa_CyrJu/Cargo.cs
+++ Projet_POO_intermediare_PotBa_CyrJu/Projet_POO_intermediare_PotBa_CyrJu/Cargo.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Projet_POO_intermediare_PotBa_CyrJu
 {
-    class Cargo
+    class Cargo : IVaisseaux
     {
+        protected int capaciteTotale;
+        protected IMateriaux pileMateriaux;
+        protected IMateriaux suivant;
+        protected IMateriaux precedent;
+        public Cargo()
+        {
+            capaciteTotale = 367;
+        }
+
+        public int CapaciteTotale
+        {
+            get { return capaciteTotale; }
+            set { capaciteTotale = value; }
+        }
+        public IMateriaux PileMateriaux
+        {
+            get { return pileMateriaux; }
+            set { pileMateriaux = value; }
+        }
+        public IMateriaux Suivant
+        {
+            get { return suivant; }
+            set { suivant = value; }
+        }
+        public IMateriaux Precedent
+        {
+            get { return precedent; }
+            set { precedent = value; }
+        }
     }
 }
